f68289b7a0490150a1bb8d20de7192877a493660
TiltedWaechter <marcel.albsmeier@gmail.com>
2021-03-24 22:28:42 +0100

    Enemy now chases player when provoked, until losing player for amount of time

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,12 +8,16 @@ public class EnemyAI : MonoBehaviour{
     //parameters
     [SerializeField] Transform target;
     [SerializeField] float chaseRange = 5;
+    [SerializeField] float timeUntilCalm = 5;
+
 
     //cached
     NavMeshAgent navMeshAgent;
     float distanceToTarget = Mathf.Infinity;
 
     //states
+    bool isProvoked = false;
+    float playerGoneTimer;
 
     private void Awake() {
         navMeshAgent = GetComponent<NavMeshAgent>();
@@ -21,10 +26,47 @@ public class EnemyAI : MonoBehaviour{
     private void Update() {
 
         distanceToTarget = Vector3.Distance(target.position, transform.position);
-        if (distanceToTarget <= chaseRange) {
-            navMeshAgent.SetDestination(target.position);
+
+        if (isProvoked) {
+            EngageTarget();
+            CalmDown();
+        } else if (distanceToTarget <= chaseRange) {
+            isProvoked = true;  
         }
         
+    }
+
+    private void CalmDown() {
+        if (distanceToTarget > chaseRange) {
+            playerGoneTimer += Time.deltaTime;
+            Debug.Log($"{name} hasn't seen {target.name} in {playerGoneTimer} seconds");
+        } else {
+            playerGoneTimer = 0;
+        }
+
+        if (playerGoneTimer >= timeUntilCalm) {
+            isProvoked = false;
+            Debug.Log($"{name} has calmed down and stopped chasing");
+        }
+    }
+
+    private void EngageTarget() {
+        if (distanceToTarget >= navMeshAgent.stoppingDistance) {
+            ChaseTarget();
+
+        }
+
+        if (distanceToTarget <= navMeshAgent.stoppingDistance) {
+            Attack();
+        }
+    }
+
+    private void Attack() {
+        Debug.Log($"{name} just hit, and is currently tearing appart {target.name}");
+    }
+
+    private void ChaseTarget() {
+        navMeshAgent.SetDestination(target.position);
     }
 
     private void OnDrawGizmosSelected() {
