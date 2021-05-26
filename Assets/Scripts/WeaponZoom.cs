7978674cde4765694887f3f05eff6e58baf49e97
TiltedWaechter <marcel.albsmeier@gmail.com>
2021-05-27 01:15:59 +0200

    zooming now changes sensitivity

--- Assets/Scripts/WeaponZoom.cs
+++ Assets/Scripts/WeaponZoom.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class WeaponZoom : MonoBehaviour{
     // Start is called before the first frame update
     [SerializeField] Camera fpsPlayerCamera;
     [SerializeField] float defaultFOV = 60f;
     [SerializeField] float zoomedInFOV = 30f;
+
+    [SerializeField] float regularSens = 2f;
+    [SerializeField] float zoomSens = 1f ;
+
+    //cached 
+    RigidbodyFirstPersonController fpsController;
+
+    private void Start() {
+        fpsController = GetComponent<RigidbodyFirstPersonController>();
+    }
 
     bool zoomToggle = false;
     private void Update() {
@@ -15,8 +26,12 @@ public class WeaponZoom : MonoBehaviour{
 
             if (zoomToggle) {
                 fpsPlayerCamera.fieldOfView = zoomedInFOV;
+                fpsController.mouseLook.XSensitivity = zoomSens;
+                fpsController.mouseLook.YSensitivity = zoomSens;
             } else {
                 fpsPlayerCamera.fieldOfView = defaultFOV;
+                fpsController.mouseLook.XSensitivity = regularSens;
+                fpsController.mouseLook.YSensitivity = regularSens;
             }
         }
     }
